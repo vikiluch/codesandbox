97ee598cdfe50f3271e167bcebda99264c3b2f38
vikiluch <80609049+vikiluch@users.noreply.github.com>
2024-04-14 13:20:22 -0700

    Initial commit

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Avatar(props) {
+  return <img className="circle-img" src={props.img} alt="Orange cat" />;
+}
+export default Avatar;
---
+++ src/contacts.js
@@ -0,0 +1,24 @@
+const contacts = [
+  {
+    name: "Cat Tom",
+    imgURL: "https://pupstoday.com/wp-content/uploads/2020/10/Orange-Cat-9.jpg",
+    phone: "+123 456 789",
+    email: "your@gmail.com"
+  },
+  {
+    name: "White dog",
+    imgURL:
+      "https://th.bing.com/th/id/R.a8cacb898162b79c5c2e828cda45c77a?rik=QuN8eoh0olBXSw&pid=ImgRaw&r=0",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    name: "Rabbit",
+    imgURL:
+      "https://th.bing.com/th/id/R.3ecf206823e4eae15ed168bbda2abd36?rik=inZhJ0c2U4VTPQ&pid=ImgRaw&r=0",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
+
+export default contacts;
